a887aef4e3cc33a1d1f089c9f8946bca08f7302b
Roberto Tejeda <emilianott19@gmail.com>
2022-11-10 00:22:07 -0400

    ultima version

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 import { servicePokemons } from "./services.js";
 import { drawPokemon } from "./drawPokemon.js";
 
-for (let id = 1; id <= 100; id++) {
+
+var x = 10
+
+for (let id = x; id <= 10; id++) {
     let servpoks = await servicePokemons(id);
-    drawPokemon(servpoks)    
+    drawPokemon(servpoks)   
+
 }
 
 
--- js/drawPokemon.js
+++ js/drawPokemon.js
@@ -1,8 +1,13 @@
-export function drawPokemon(servpoks) {
+
+
+
+
+export function drawPokemon(servpoks ) {
+    
 
     // Tarjetas
     const cards = document.createElement("div");
-    cards.classList.add("card", "bg-light", "border-light", "rounded-5", "cards");
+    cards.classList.add("card", "border-light", "rounded-5", "cards","h-100");
 
     // Coloumnas
     const cols = document.createElement("div");
@@ -16,7 +21,7 @@ export function drawPokemon(servpoks) {
     // Imagen
     const cardImage = document.createElement("img");
     cardImage.src = servpoks.sprites.front_default;
-    cardImage.classList.add("rounded-5", "mx-auto", "d-block", "h-100", "cardImage", "mb-5");
+    cardImage.classList.add("mx-auto", "d-block", "h-90", "cardImage", "mb-5");    
     cards.appendChild(cardImage);
 
     // Nombre
@@ -25,20 +30,61 @@ export function drawPokemon(servpoks) {
     cardName.classList.add("fw-bold", "text-center", "mb-5", "cardName");
     cards.appendChild(cardName);
 
+    //Pokemon Type & Img Color
+    for (let i = 0; i < servpoks.types.length; i++) {
+        const cardAbilities = document.createElement("p");
+        cardAbilities.innerHTML = `Types : <span class="fw-bold"> ${servpoks.types[i].type.name} </span>`
+        cardAbilities.classList.add("text-center", "cardAbilities")
+        if(servpoks.types[0].type.name ==="grass"){
+            cardImage.style.backgroundColor ="#B8FF33"    
+        }
+        else if(servpoks.types[0].type.name ==="fire"){
+            cardImage.style.backgroundColor ="#FB6746"
+        }
+        else if(servpoks.types[0].type.name ==="water"){
+            cardImage.style.backgroundColor ="#69A1FC"
+        }
+        else if(servpoks.types[0].type.name ==="bug"){
+            cardImage.style.backgroundColor ="#5A4A55"
+        }
+        else if(servpoks.types[0].type.name ==="poison"){
+            cardImage.style.backgroundColor ="purple"
+        }
+        else if(servpoks.types[0].type.name ==="normal"){
+            cardImage.style.backgroundColor ="white"
+        }
+        else if(servpoks.types[0].type.name ==="ghost"){
+            cardImage.style.backgroundColor ="#061011"
+        }
+        else if(servpoks.types[0].type.name ==="electric"){
+            cardImage.style.backgroundColor ="#E0FF00"
+        }
+        else if(servpoks.types[0].type.name ==="psychic"){
+            cardImage.style.backgroundColor ="#5C5887"
+        }
+        else if(servpoks.types[0].type.name ==="rock"){
+            cardImage.style.backgroundColor ="darkgrey"
+        }
+             
+        
+        
+        cards.appendChild(cardAbilities)
+    }
+    
+    
     // Habilidades
-    for (let i = 0; i < servpoks.abilities.length; i++) {
+    for (let i = 1; i < servpoks.abilities.length; i++) {
+        
         const cardAbilities = document.createElement("p");
         cardAbilities.innerHTML = `Ability ${i + 1}: <span class="fw-bold"> ${servpoks.abilities[i].ability.name} </span>`
         cardAbilities.classList.add("text-center", "cardAbilities")
         cards.appendChild(cardAbilities)
     }
-
-
-
-
-
-
-
+    
+    const pokemonId = document.createElement("p")
+    pokemonId.classList.add("pokeId")
+    pokemonId.innerHTML = servpoks.id
+    cards.appendChild(pokemonId)
 
 
 
